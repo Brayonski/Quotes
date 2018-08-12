a1a2bd9eded59b79e85e50a6628ce8a3d6286b0e
root <root@localhost.localdomain>
2018-08-12 15:27:23 +0300

    posting publisher

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote'
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
+  quotePublisher:string
+  myQuote:any
+  @Output() emitQuote = new EventEmitter();
+  submitQuote(){
+    this.myQuote= new Quote(this.quotePublisher)
+    this.emitQuote.emit(this.myQuote)
+  }
   constructor() { }
 
   ngOnInit() {
